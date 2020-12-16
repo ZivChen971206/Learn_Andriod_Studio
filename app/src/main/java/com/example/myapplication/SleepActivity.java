42f00559eb84483f802b0dfa0eef7bdb5f386bcc
ZivChen971206 <932483811@qq.com>
2020-12-16 10:42:25 +0800

    完成选框和书籍样式

--- app/src/main/java/com/example/myapplication/SleepActivity.java
+++ app/src/main/java/com/example/myapplication/SleepActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.myapplication.Adapter.PlayAdapter;
 
 public class SleepActivity extends AppCompatActivity {
+    private RecyclerView mRVFinish;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+        mRVFinish = findViewById(R.id.rv_finish);
+        //利用adapter显示item
+        mRVFinish.setLayoutManager(new LinearLayoutManager(SleepActivity.this));
+        //设置adapter
+        mRVFinish.setAdapter(new PlayAdapter(SleepActivity.this, new PlayAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int pos) {
+                Toast.makeText(SleepActivity.this, "click..." + pos, Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/StudyActivity.java
+++ app/src/main/java/com/example/myapplication/StudyActivity.java
@@ -3,12 +3,40 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class StudyActivity extends AppCompatActivity {
+
+    private RadioGroup mRG1;
+    private CheckBox mCB1;
+    private CheckBox mCB2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
+
+        mRG1 = findViewById(R.id.rg_1);
+        mCB1 = findViewById(R.id.cb_1);
+        mCB2 = findViewById(R.id.cb_2);
+
+        mRG1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                Toast.makeText(StudyActivity.this,radioButton.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCB1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               Toast.makeText(StudyActivity.this,isChecked?"选中":"未选中",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
